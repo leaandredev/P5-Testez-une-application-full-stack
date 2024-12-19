ffb4d07f972b349bda06ee4c28e506531f522edb
Lea ANDRE <aleaandre@gmail.com>
2024-12-19 16:09:00 +0100

    [AF-233] : integration tests

--- front/src/app/components/me/me.component.spec.ts
+++ front/src/app/components/me/me.component.spec.ts
@@ -1,11 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import {
-  ComponentFixture,
-  fakeAsync,
-  TestBed,
-  tick,
-  waitForAsync,
-} from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -19,12 +13,23 @@ import { of } from 'rxjs';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { Router } from '@angular/router';
 
+import { fireEvent, screen } from '@testing-library/angular';
+import '@testing-library/jest-dom';
+
 describe('MeComponent', () => {
   let component: MeComponent;
   let fixture: ComponentFixture<MeComponent>;
 
-  const userMock = {
+  //Mock datas
+  const mockUser = {
     id: 1,
+    email: 'test@yoga.fr',
+    lastName: 'LASTNAME',
+    firstName: 'FirstName',
+    admin: true,
+    password: 'fjhzbefhbhbbhgz55655',
+    createdAt: '2024/12/12',
+    updatedAt: '2024/12/12',
   };
 
   // Mock services
@@ -37,7 +42,7 @@ describe('MeComponent', () => {
   };
 
   const mockUserService = {
-    getById: jest.fn().mockReturnValue(of(userMock)),
+    getById: jest.fn().mockReturnValue(of(mockUser)),
     delete: jest.fn().mockReturnValue(of(null)),
   };
 
@@ -46,7 +51,8 @@ describe('MeComponent', () => {
   };
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
+    jest.clearAllMocks();
+    TestBed.configureTestingModule({
       declarations: [MeComponent],
       imports: [
         MatSnackBarModule,
@@ -63,10 +69,13 @@ describe('MeComponent', () => {
         { provide: Router, useValue: mockRouter },
       ],
     }).compileComponents();
-
-    jest.clearAllMocks();
     fixture = TestBed.createComponent(MeComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
     fixture.detectChanges();
   });
 
@@ -74,45 +83,121 @@ describe('MeComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // initialisation
-  describe('on initialisation', () => {
-    it('should call userService.getById', () => {
-      expect(mockUserService.getById).toHaveBeenCalledTimes(1);
+  // Integration tests
+  describe('integration test', () => {
+    it('should display Name paragraph with right datas', async () => {
+      const nameParagraph = await screen.findByText('Name: FirstName LASTNAME');
+      expect(nameParagraph).toBeInTheDocument();
     });
 
-    it('should fill user', () => {
-      expect(component.user).toBe(userMock);
+    it('should display Email paragraph', async () => {
+      const nameParagraph = await screen.findByText('Email: test@yoga.fr');
+      expect(nameParagraph).toBeInTheDocument();
     });
 
-    it('should be same user id that sessionInformation', () => {
-      expect(component.user?.id).toBe(mockSessionService.sessionInformation.id);
+    it('should call back function on back button click', async () => {
+      const backSpyOn = jest.spyOn(component, 'back');
+      const buttons = screen.queryAllByRole('button');
+      const backButton = buttons.find((btn) =>
+        btn.querySelector('mat-icon')?.textContent?.includes('arrow_back')
+      );
+
+      fireEvent.click(backButton as HTMLElement);
+      expect(backSpyOn).toHaveBeenCalledTimes(1);
+    });
+
+    // User in an admin
+    describe('user is an admin', () => {
+      beforeEach(async () => {
+        mockUser.admin = true;
+        mockSessionService.sessionInformation.admin = true;
+        fixture.detectChanges();
+      });
+
+      it('should display "You are admin" paragraph', async () => {
+        const nameParagraph = await screen.findByText('You are admin');
+        expect(nameParagraph).toBeInTheDocument();
+      });
+
+      it('should not display button to delete account', async () => {
+        const deleteButton = await screen.queryByText('Detail');
+        expect(deleteButton).not.toBeInTheDocument();
+      });
+    });
+
+    // User is not an admin
+    describe('user is not an admin', () => {
+      beforeEach(async () => {
+        mockUser.admin = false;
+        mockSessionService.sessionInformation.admin = false;
+        fixture.detectChanges();
+      });
+
+      it('should not display "You are admin" paragraph', async () => {
+        const nameParagraph = screen.queryByText('You are admin');
+        expect(nameParagraph).not.toBeInTheDocument();
+      });
+
+      it('should display button to delete account', async () => {
+        const deleteButton = screen.queryByText('Detail');
+        expect(deleteButton).toBeInTheDocument();
+      });
+
+      it('should call delete function on delete button click', async () => {
+        const deleteSpyOn = jest.spyOn(component, 'delete');
+        const deleteButton = screen.queryByText('Detail');
+        expect(deleteButton).toBeInTheDocument();
+
+        fireEvent.click(deleteButton as HTMLElement);
+        expect(deleteSpyOn).toHaveBeenCalledTimes(1);
+      });
     });
   });
 
-  // back()
-  it('should call window.history.back on back() call', () => {
-    const windowHistoryBackSpy = jest.spyOn(window.history, 'back');
-    component.back();
-    expect(windowHistoryBackSpy).toHaveBeenCalled();
-  });
+  // Unit tests
+  describe('unit test', () => {
+    // ngOnInit()
+    describe('on initialisation', () => {
+      it('should call userService.getById', () => {
+        expect(mockUserService.getById).toHaveBeenCalledTimes(1);
+      });
 
-  // delete()
-  describe('on delete() call', () => {
-    beforeEach(waitForAsync(() => {
-      component.delete();
-      fixture.whenStable();
-    }));
+      it('should fill user', () => {
+        expect(component.user).toBe(mockUser);
+      });
 
-    it('should call userService.delete', () => {
-      expect(mockUserService.delete).toHaveBeenCalledTimes(1);
+      it('should be same user id that sessionInformation', () => {
+        expect(component.user?.id).toBe(
+          mockSessionService.sessionInformation.id
+        );
+      });
     });
 
-    it('should call sessionService.logOut after userService.delete', () => {
-      expect(mockSessionService.logOut).toHaveBeenCalledTimes(1);
+    // back()
+    it('should call window.history.back on back() call', () => {
+      const windowHistoryBackSpy = jest.spyOn(window.history, 'back');
+      component.back();
+      expect(windowHistoryBackSpy).toHaveBeenCalled();
     });
 
-    it('should navigate to "/" after user deletion', () => {
-      expect(mockRouter.navigate).toHaveBeenCalledWith(['/']);
+    // delete()
+    describe('on delete() call', () => {
+      beforeEach(waitForAsync(() => {
+        component.delete();
+        fixture.whenStable();
+      }));
+
+      it('should call userService.delete', () => {
+        expect(mockUserService.delete).toHaveBeenCalledTimes(1);
+      });
+
+      it('should call sessionService.logOut after userService.delete', () => {
+        expect(mockSessionService.logOut).toHaveBeenCalledTimes(1);
+      });
+
+      it('should navigate to "/" after user deletion', () => {
+        expect(mockRouter.navigate).toHaveBeenCalledWith(['/']);
+      });
     });
   });
 });

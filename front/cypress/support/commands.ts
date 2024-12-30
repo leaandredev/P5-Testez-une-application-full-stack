6d2b029d79afde75831d777ca5321c70e901e730
Lea ANDRE <aleaandre@gmail.com>
2024-12-30 16:13:19 +0100

    [AF-286] : add commands for cy.intercept with fixtures, and register steps

--- front/cypress/support/commands.ts
+++ front/cypress/support/commands.ts
@@ -42,8 +42,50 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('login', (email, password) => {
-  cy.visit('/login');
+Cypress.Commands.add('interceptWithFixture', (method, url, fixturePath) => {
+  cy.fixture(fixturePath).then((data) => {
+    cy.intercept(
+      {
+        method: method,
+        url: url,
+      },
+      data
+    ).as(fixturePath); // Utilise le chemin comme alias pour retrouver l'interception
+  });
+});
+
+Cypress.Commands.add('initIntercepts', () => {
+  //get all
+  cy.interceptWithFixture('GET', '/api/session', 'sessions');
+  cy.interceptWithFixture('GET', '/api/user', 'users');
+  cy.interceptWithFixture('GET', '/api/teacher', 'teachers');
+
+  // get user by id
+  cy.fixture('users').then((users) => {
+    cy.intercept('GET', '/api/user/*', (req) => {
+      const userId = Number(req.url.split('/').pop());
+      const user = users.find((u) => u.id === userId);
+      req.reply(user ? user : { error: 'User not found' });
+    }).as('getUser');
+  });
+
+  cy.fixture('sessions').then((sessions) => {
+    cy.intercept('GET', '/api/session/*', (req) => {
+      const sessionId = Number(req.url.split('/').pop());
+      const session = sessions.find((s) => s.id === sessionId);
+      req.reply(session ? session : { error: 'Session not found' });
+    }).as('getSession');
+  });
+
+  cy.fixture('teachers').then((teachers) => {
+    cy.intercept('GET', '/api/teacher/*', (req) => {
+      const teacherId = Number(req.url.split('/').pop());
+      const teacher = teachers.find((t) => t.id === teacherId);
+      req.reply(teacher ? teacher : { error: 'Teacher not found' });
+    }).as('getTeacher');
+  });
+
+  // login response success
   cy.intercept('POST', '/api/auth/login', {
     body: {
       id: 1,
@@ -52,15 +94,26 @@ Cypress.Commands.add('login', (email, password) => {
       lastName: 'lastName',
       admin: true,
     },
-  });
+  }).as('login');
 
-  cy.intercept(
-    {
-      method: 'GET',
-      url: '/api/session',
+  // register response success
+  cy.intercept('POST', '/api/auth/register', {
+    body: {
+      message: 'User registered successfully!',
     },
-    []
-  ).as('session');
+  }).as('register');
+});
+
+Cypress.Commands.add('login', (email, password) => {
+  cy.visit('/login');
+  cy.get('input[formControlName=email]').type(email);
+  cy.get('input[formControlName=password]').type(`${password}{enter}{enter}`);
+});
+
+Cypress.Commands.add('register', (firstName, lastName, email, password) => {
+  cy.visit('/register');
+  cy.get('input[formControlName=firstName]').type(firstName);
+  cy.get('input[formControlName=lastName]').type(lastName);
   cy.get('input[formControlName=email]').type(email);
   cy.get('input[formControlName=password]').type(`${password}{enter}{enter}`);
 });
--- front/cypress/support/index.d.ts
+++ front/cypress/support/index.d.ts
@@ -7,5 +7,17 @@ declare namespace Cypress {
      * @example cy.login('email@example.com', 'password123')
      */
     login(email: string, password: string): Chainable<void>;
+    register(
+      firstName: string,
+      lastName: string,
+      email: string,
+      password: string
+    ): Chainable<void>;
+    initIntercepts(): Chainable<void>;
+    interceptWithFixture(
+      method: string,
+      url: string,
+      fixturePath: string
+    ): Chainable<void>;
   }
 }

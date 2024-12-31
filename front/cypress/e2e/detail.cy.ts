be9904602c7db8dff58f3aa3b1f1715716357739
Lea ANDRE <aleaandre@gmail.com>
2024-12-31 10:28:28 +0100

    [AF-286] : handle no user found

--- front/cypress/e2e/detail.cy.ts
+++ front/cypress/e2e/detail.cy.ts
@@ -1,11 +1,23 @@
 describe('Detail spec', () => {
   beforeEach(() => {
     cy.initIntercepts();
+    cy.login('yoga@studio.com', 'test!1234');
   });
 
   it('should display infos successfully', () => {
-    cy.login('yoga@studio.com', 'test!1234');
     cy.contains('Account').click();
     cy.url().should('include', '/me');
+    cy.contains('Name: Admin ADMIN');
+    cy.contains('Email: yoga@studio.com');
+    cy.contains('You are admin');
+  });
+
+  it('should not display infos if no user found', () => {
+    cy.intercept('GET', '/api/user/1', {
+      statusCode: 404,
+    });
+    cy.contains('Account').click();
+    cy.url().should('include', '/me');
+    cy.get('mat-card-content').should('be.empty');
   });
 });
